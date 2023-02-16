09075bee4169461ca7428d4c590ba238992c78f8
Maroof Ali <maroofalimehdi@gmail.com>
2023-02-16 14:52:35 +0500

    changes color

--- src/main-pages/Birds.js
+++ src/main-pages/Birds.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 
 const Birds = () => {
   const { name } = useParams();
-  return <h1 className="text-white">Birds {name}</h1>;
+  return <h1 className="text-yellow-400">Birds {name}</h1>;
 };
 
 export default Birds;
